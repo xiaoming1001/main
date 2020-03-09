cc05dcdfdbaebce4e5fe1b523c68d2d0606b13fb
admin <zengming1118>
2020-03-09 22:55:11 +0800

    第5次提交

--- src/main/java/com/study/shiwu/ShiwuApplication.java
+++ src/main/java/com/study/shiwu/ShiwuApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication   //测试代码git吹送
 @EnableTransactionManagement   //不写也可以，Springboot会自动配置
 public class ShiwuApplication {
 
